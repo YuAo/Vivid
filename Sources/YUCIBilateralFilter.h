4b2e086ea88ff8d937ab8e075539bafb749a9e26
YuAo <me@imyuao.com>
2016-02-03 13:32:17 +0800

    improves bilateral filter

--- Sources/YUCIBilateralFilter.h
+++ Sources/YUCIBilateralFilter.h
@@ -12,4 +12,8 @@
 
 @property (nonatomic,strong) CIImage *inputImage;
 
+@property (nonatomic,copy) NSNumber *inputRadius; //default 10
+@property (nonatomic,copy) NSNumber *inputDistanceNormalizationFactor; //default 6.0
+@property (nonatomic,copy) NSNumber *inputTexelSpacingMultiplier; //default 1.0
+
 @end
